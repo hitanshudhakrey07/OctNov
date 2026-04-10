bf1000bd6649bbc783ab0f15874b9fedfbff8281
hitanshudhakrey07 <hitanshudhakrey07@gmail.com>
2026-04-10 13:28:35 +0530

    commit

---
+++ GLA/08-04-26/seed.js
@@ -0,0 +1,17 @@
+let p=[
+    {
+        name:"Phone",
+        price:30000,
+        rating:3.5,
+        img:"https://images.unsplash.com/photo-1580910051074-3eb694886505?q=80&w=465&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description :"LEICA CAMERA AG, LEICA Q (Typ 116)"
+    },
+    {
+         name:"laptop",
+        price:80000,
+        rating:4.5,
+        img:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description :"Canon, EOS 6D"
+    }
+];
+module.exports=p;
